b06d415774352f0f40fd69bec576fedb4dfc5185
Saeed473 <isaeedrana9@gmail.com>
2025-04-20 22:56:33 -0700

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,17 @@
+import AdminDashboard from "./components/Dashboard/AdminDashboard"
+import EmployeeDashboard from "./components/Dashboard/EmployeeDashboard"
+
+
+const App = () => {
+
+  
+  return (
+    <div>
+     {/* <AdminDashboard /> */}
+     <EmployeeDashboard />
+    </div>
+  )
+}
+
+export default App
+
